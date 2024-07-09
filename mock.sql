4031bf0f27b565fd00f537721e23fc0712c3756f
fsirinco278 <fs.irinco@gmail.com>
2024-07-09 13:15:29 +1200

    update mock SQL query

--- mock.sql
+++ mock.sql
@@ -1,9 +1,13 @@
-SELECT MIN(t.title) AS movie_title,
+SELECT MIN(mc.note) AS production_note,
+       MIN(t.title) AS movie_title,
        MIN(t.production_year) AS movie_year
-FROM movie_info AS mi,
+FROM title AS t,
+     movie_info AS mi,
      movie_info_idx AS mi_idx,
-     title AS t
+     movie_link AS ml, 
+     complete_cast AS cc
 WHERE t.production_year BETWEEN 2005 AND 2010
   AND t.id = mi_idx.movie_id
-  AND t.id = mc.movie_id
-  AND mc.movie_id = mi_idx.movie_id
+  AND t.id = mi.movie_id
+  AND t.id = ml.movie_id
+  AND t.id = cc.movie_id
